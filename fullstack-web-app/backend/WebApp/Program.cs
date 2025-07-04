86961439a556ba6357be0d761e39615555097561
Egor <egor_udir@mail.ru>
2025-07-04 17:47:59 +0300

    added fullstack-web-app (asp+react)

---
+++ fullstack-web-app/backend/WebApp/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000");
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection(); 
+app.UseCors();
+app.MapControllers();
+
+app.Run();
