b99455fcb420e6edfed6ea4065867ef650464761
Egor <egor_udir@mail.ru>
2025-07-07 13:19:09 +0300

    backend target uri is taken from docker-compose file

--- full-fullstack-web-app-graphql/frontend/vite.config.ts
+++ full-fullstack-web-app-graphql/frontend/vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const backendTarget = process.env.API_TARGET || 'http://localhost:5000';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://backend:8080',
+      '/graphql': {
+        target: backendTarget,
         changeOrigin: true,
         secure: false,
       },
     },
-  }
+  },
 })
--- fullstack-web-app/frontend/vite.config.ts
+++ fullstack-web-app/frontend/vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const backendTarget = process.env.API_TARGET || 'http://localhost:5000';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://backend:8080', // http://localhost:5000, if starts from the IDE
+        target: backendTarget,
         changeOrigin: true,
         secure: false,
       },
     },
-  }
+  },
 })
